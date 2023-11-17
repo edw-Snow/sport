2765767748a540f1d17bb515d0d51ec5f7be246c
beichen <3072338636@qq.com>
2023-11-17 11:26:25 +0800

    项目目录 first commit

---
+++ src/main/java/com/sport/SportAckendApplication.java
@@ -0,0 +1,14 @@
+package com.sport;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportAckendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportAckendApplication.class, args);
+    }
+
+}
