44ee60855612a11d633ebfb11b0267f0717c77ad
beichen <3072338636@qq.com>
2023-11-18 12:09:26 +0800

    jwt令牌校验 first commit

--- src/test/java/com/sport/SportAckendApplicationTests.java
+++ src/test/java/com/sport/SportAckendApplicationTests.java
@@ -3,11 +3,14 @@ package com.sport;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SportAckendApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+    }
 }
